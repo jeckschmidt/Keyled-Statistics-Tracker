98662085d91bace2a51a56ddca83de66f318f98b
jeckschmidt <jackie11418@gmail.com>
2025-01-12 12:09:15 -0500

    Included sql file for database creation

---
+++ keyled_db.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS `Keyled_Statistics`;
+CREATE DATABASE `Keyled_Statistics`;
+USE `Keyled_Statistics`;
+
+CREATE TABLE target_information (
+	id INT AUTO_INCREMENT,
+	serial_number VARCHAR (50),
+    flash_status BOOL,
+    bytes_written INT,
+    program_version VARCHAR (50),
+    target_RTC VARCHAR (50),
+    flash_date VARCHAR (50),
+    realtime_RTC_difference VARCHAR (50),
+	PRIMARY KEY (id)
+);
