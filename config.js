f1259fa0feaf0106b8da1d6f52dd693446305ea4
jeckschmidt <jackie11418@gmail.com>
2026-01-26 18:42:33 -0500

    added more config details

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv/config'
+import path from 'path'
 
 export const app = {
     port: process.env.PORT,
@@ -6,5 +7,6 @@ export const app = {
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    table: process.env.MYSQL_TABLE
+    table: process.env.MYSQL_TABLE,
+    csvLocation: `${path.resolve()}/public`
 }
