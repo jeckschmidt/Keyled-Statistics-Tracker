3bf42e3b04d6bd034548c4abe4c38c7941b707c9
jeckschmidt <jackie11418@gmail.com>
2026-01-26 14:28:22 -0500

    included all environment variables

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv/config'
 
 export const app = {
+    port: process.env.PORT,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    table: process.env.MYSQL_TABLE
 }
